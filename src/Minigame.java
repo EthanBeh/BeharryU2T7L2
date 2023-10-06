78726fa9d87a10324319791394bdb1ac0cc07fa9
BT_1N3_02 <sample@bths.edu>
2023-10-06 09:15:13 -0400

    minigame is the only one I really care about

--- src/Minigame.java
+++ src/Minigame.java
@@ -1,2 +1,39 @@
-package PACKAGE_NAME;public class Minigame {
+import java.util.Scanner;
+
+public class Minigame {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Please enter your first word: ");
+        String word = scan.nextLine();
+        int score = 0;
+        String wordPrevious;
+        int numWords = 1;
+        System.out.println();
+        while (score < 50) {
+            wordPrevious = word;
+            System.out.print("Please enter your next word: ");
+            word = scan.nextLine();
+            if (word.compareTo(wordPrevious) > 0) {
+                score += 2;
+            } else if (word.compareTo(wordPrevious) < 0) {
+                score -= 5;
+            } else {
+                score -= 10;
+            }
+            if (wordPrevious.substring(wordPrevious.length()-2).equals(word.substring(0, 2))) {
+                score += 5;
+            }
+            if (word.indexOf(wordPrevious.substring(0,1)) != -1) {
+                score += 3;
+            }
+            if (word.indexOf(wordPrevious.substring(0, 1)) <= 1 || word.indexOf(wordPrevious.substring(1, 2)) <= 1) {
+                score -= 3;
+            }
+            System.out.println("Your score is now " + score);
+            System.out.println();
+            numWords++;
+         }
+        System.out.println("Congratulations! You win!");
+        System.out.println("It took you " + numWords + " words to win.");
+    }
 }
--- src/Yippee.java
+++ src/Yippee.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class Yippee {
+import java.util.Scanner;
+public class Yippee {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter string 1: ");
+        String s1 = scan.nextLine();
+        System.out.print("Enter string 2: ");
+        String s2 = scan.nextLine();
+        if (s1.equals(s2)) {
+            System.out.println("The strings are equal!");
+        } else {
+            System.out.println("The strings are NOT equal");
+            if (s1.compareTo(s2) > 0) {
+                System.out.println(s1 + " comes before " + s2 + " alphabetically");
+            } else {
+                System.out.println(s2 + " comes before " + s1 + " alphabetically");
+            }
+        }
+    }
 }
